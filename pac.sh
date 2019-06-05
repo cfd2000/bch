b5d24f762b943ab8698c7479272b91b434f252de
damang2 <50958605+damang2@users.noreply.github.com>
2019-06-05 11:19:24 +0800

    Create pac.sh

---
+++ pac.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+pkg install update
+pkg install upgrade
+apt update
+apt upgrade -y
+pkg install python -y
+pip install --upgrade pip
+pip install --r requirements
+
+echo -e "\e[1m\e[31mWARNING donot Subscribe :\e[32m Termux Tricks & Tut"
+echo
+echo -e "\e[1m\e[31m ENTER :\e[32m exit"
+echo
